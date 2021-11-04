ad86f860d36942cca19252f44b99ad2aca708d72
oollie2 <olliesharratt34@gmail.com>
2021-11-04 17:26:54 +0000

    initial upload of project

---
+++ src/Classes/OSFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Vigilate
+{
+    public static class OSFunctions
+    {
+        public static class Sleep
+        {
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+            public static void PreventSleep()
+            {
+                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                    | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                    | EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+            }
+            public static void AllowSleep()
+            {
+                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+            }
+
+            [Flags]
+            private enum EXECUTION_STATE : uint
+            {
+                ES_SYSTEM_REQUIRED = 0x00000001,
+                ES_DISPLAY_REQUIRED = 0x00000002,
+                ES_AWAYMODE_REQUIRED = 0x00000040,
+                ES_CONTINUOUS = 0x80000000,
+            }
+        }
+    }
+}
---
+++ src/ProjectInstaller.Designer.cs
@@ -0,0 +1,62 @@
+﻿using System.ServiceProcess;
+
+namespace Vigilate
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private ServiceInstaller serviceInstaller;
+        private ServiceProcessInstaller processInstaller;
+        private void InitializeComponent()
+        {
+            this.vigilateServiceInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.vigilateService = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // vigilateServiceInstaller
+            // 
+            this.vigilateServiceInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.vigilateServiceInstaller.Password = null;
+            this.vigilateServiceInstaller.Username = null;
+            // 
+            // vigilateService
+            // 
+            this.vigilateService.Description = "GA Prevent PC from locking";
+            this.vigilateService.DisplayName = "Vigilate Service";
+            this.vigilateService.ServiceName = "Vigilate";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.vigilateServiceInstaller,
+            this.vigilateService});
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller vigilateServiceInstaller;
+        private System.ServiceProcess.ServiceInstaller vigilateService;
+    }
+}
