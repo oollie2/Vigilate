0e0a93311db8d2dd2a12b1e34d366ee5afc79d27
oollie34 <olliesharratt34@gmail.com>
2023-10-02 21:22:49 +0100

    removed the requirement for newtonsoft.json and split settings classes

---
+++ src/Vigilate.Core/VigilateSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Vigilate.Core;
+
+public class VigilateSettings
+{
+    #region PreviousState
+    internal bool _previousState = false;
+    public bool State
+    {
+        get { return _previousState; }
+        set { _previousState = value; }
+    }
+    #endregion
+    #region PreviousState
+    internal int _pollPeriodMs = 30000;
+    public int PollPeriodMs
+    {
+        get { return _pollPeriodMs; }
+        set { _pollPeriodMs = value; }
+    }
+    #endregion
+}
